439f11d64b9354141e92ed17e02002a8dbc9dded
jiaren <jia.ren7@gmail.com>
2022-01-29 17:43:15 -0800

    initial commit

--- Assignments/src/neu/jia/Main.java
+++ Assignments/src/neu/jia/Main.java
@@ -1,8 +1,8 @@
-package edu.northeastern.jia;
+package neu.jia;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+	// write your code here
     }
 }
