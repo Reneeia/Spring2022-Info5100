dc397f5fbdb9614e776506ce69e1ab47f6b5916b
jiaren <jia.ren7@gmail.com>
2022-02-15 15:37:38 -0800

    Info5100 assignment02 some corrections

--- Assignments/src/neu/jia/Main.java
+++
@@ -1,8 +0,0 @@
-package neu.jia;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- Assignments/src/neu/jia/assignment02/question03/ShapeTester.java
+++ Assignments/src/neu/jia/assignment02/question03/ShapeTester.java
@@ -1,6 +1,6 @@
 package neu.jia.assignment02.question03;
 
-public class shapeTester {
+public class ShapeTester {
     public static void main(String[] args) {
         Shape s = new Shape();
         Rectangle r = new Rectangle(2.0, 3.0);
