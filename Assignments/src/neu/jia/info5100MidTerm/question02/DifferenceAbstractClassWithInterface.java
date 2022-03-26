bac808882dade1d6f6abd686aed08729f6603e74
jiaren <jia.ren7@gmail.com>
2022-03-26 16:27:09 -0700

    some check in some corrections of info5100 midterm

--- Assignments/src/neu/jia/info5100MidTerm/question02/DifferenceAbstractClassWithInterface.java
+++ Assignments/src/neu/jia/info5100MidTerm/question02/DifferenceAbstractClassWithInterface.java
@@ -7,20 +7,21 @@ public class DifferenceAbstractClassWithInterface {
     //Abstract class
 
     //1.a class can only extents one abstract class
-    //2.abstract class can have fields
+    //2.abstract class can define fields
     //3.abstract class can have access modifiers
     //4.have abstract methods and concrete method
     //5.as for speed: abstract class is fast
     //6.an abstract class can be extended
     //7.an abstract class can have public and protected method
     //8.abstract class can inheritance interfaces and class
-    //9.abstract method is a must
+    //9.abstract class need abstract method
+    //10.in abstract class, code can be provided
 
 
     //Interface:
 
     //1.a class can implements multiple interfaces
-    //2.interface do not have fields and could only have methods that need to be override
+    //2.interface can not define fields and could only have methods that need to be overridden
     //3.interface do not have access modifiers
     //4.have abstract methods only
     //5.as for speed: interface is slow
@@ -28,4 +29,5 @@ public class DifferenceAbstractClassWithInterface {
     //7.interface can only have public methods
     //8.interface can inherit interface but not class
     //9.abstract method is not necessary
+    //10.interface cannot provide any code
 }
--- Assignments/src/neu/jia/info5100MidTerm/question05/Camera.java
+++ Assignments/src/neu/jia/info5100MidTerm/question05/Camera.java
@@ -2,27 +2,23 @@ package neu.jia.info5100MidTerm.question05;
 
 public class Camera {
 
-
     private static Camera _instance;
-    private static Object obj;
+    private static Object obj = new Object();
     private String cameraName = "";
 
-    //constructor without parameter
     private Camera() {
 
     }
 
-    //getter
     public String getCameraName() {
         return cameraName;
     }
 
-    //setter
     public void setCameraName(String cameraName) {
         this.cameraName = cameraName;
     }
 
-    //public getInstance method can only create one Camera instance
+
     public static Camera getInstance() {
         if (_instance == null) {
             synchronized (obj) {
