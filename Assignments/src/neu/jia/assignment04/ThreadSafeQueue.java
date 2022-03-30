83e7329e8990a78f43f54ed2d3e685a6f743dc9e
jiaren <jia.ren7@gmail.com>
2022-03-30 16:46:11 -0700

    check in assign04

---
+++ Assignments/src/neu/jia/assignment04/ThreadSafeQueue.java
@@ -0,0 +1,38 @@
+package neu.jia.assignment04;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ThreadSafeQueue<T> {
+    private Queue<T> threadSafeQueue;
+    public Object objLock = new Object();
+
+    //constructor
+    public ThreadSafeQueue() {
+        threadSafeQueue = new LinkedList<>();
+    }
+
+    public void add(T data) {
+        synchronized (objLock) {
+            threadSafeQueue.add(data);
+        }
+    }
+
+    public T remove() {
+        synchronized (objLock) {
+            return threadSafeQueue.remove();
+        }
+    }
+
+    public T peek() {
+        synchronized (objLock) {
+            return threadSafeQueue.remove();
+        }
+    }
+
+    public boolean isEmpty() {
+        synchronized (objLock) {
+            return threadSafeQueue.peek() == null;
+        }
+    }
+}
